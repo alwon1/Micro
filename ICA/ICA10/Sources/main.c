ceb967eb7d7090d6ec56d74d7c858dabf8b5873b
Allyn Nelson <allynnelson13@gmail.com>
2019-10-20 20:19:22 -0600

    all but last of ica done just need to test

--- ICA/ICA10/Sources/main.c
+++ ICA/ICA10/Sources/main.c
@@ -1,15 +1,21 @@
+#include "SCI0.h"
 #include "derivative.h" /* derivative-specific definitions */
 #include <hidef.h>		/* common defines and macros */
-
 void main(void)
 {
 	/* put your own code here */
 
-	EnableInterrupts;
-
+	//EnableInterrupts;
+	SCI0_Init19200();
+	SCI0_TxString("Welcome to the world of serial communication\r\n");
 	for (;;)
 	{
-		_FEED_COP(); /* feeds the dog */
-	}				 /* loop forever */
-					 /* please make sure that you never leave main */
+		char x;
+		if ((x = SCI0_RxChar()))
+		{
+			SCI0_TxChar(x);
+		}
+		//_FEED_COP(); /* feeds the dog */
+	} /* loop forever */
+	  /* please make sure that you never leave main */
 }
