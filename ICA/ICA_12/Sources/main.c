a7bdcde1347bfb1587eaa51c6884cab6042f2fa9
Allyn Nelson <allynnelson13@gmail.com>
2019-10-29 11:51:36 -0600

    Rx interupt works in this

--- ICA/ICA_12/Sources/main.c
+++ ICA/ICA_12/Sources/main.c
@@ -1,21 +1,34 @@
+#include "derivative.h" /* derivative-specific definitions */
 #include <hidef.h>      /* common defines and macros */
-#include "derivative.h"      /* derivative-specific definitions */
+//#define interrupt
+//#define VectorNumber_Vsci0
+#include "SCI0.h"
 
+void _Vsci0_RxItr(void);
 
-
-
-
-
-void main(void) {
+void main(void)
+{
   /* put your own code here */
-  
 
-
-	EnableInterrupts;
-
-
-  for(;;) {
-    _FEED_COP(); /* feeds the dog */
-  } /* loop forever */
+  _DISABLE_COP();
+  SCI0_Init19200();
+  EnableInterrupts;
+  SCI0CR2_RIE = 1;
+  for (;;)
+  {
+    asm wai;
+    //_FEED_COP(); /* feeds the dog */
+  }              /* loop forever */
   /* please make sure that you never leave main */
+}
+
+interrupt VectorNumber_Vsci0 void _Vsci0_RxItr(void)
+{
+  char temp;
+  if (SCI0SR1_RDRF)
+  {
+    temp = SCI0DRL;
+    SCI0DRL = temp;
+  }
+  return;
 }
