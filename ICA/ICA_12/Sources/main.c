416adc4665b4dbc7a107d857e826fd647dbfa756
Allyn Nelson <allynnelson13@gmail.com>
2019-10-31 12:33:07 -0600

    ICA 12 Done

--- ICA/ICA_12/Sources/main.c
+++ ICA/ICA_12/Sources/main.c
@@ -2,23 +2,25 @@
 #include <hidef.h>      /* common defines and macros */
 //#define interrupt
 //#define VectorNumber_Vsci0
+#include "Delay.h"
+#include "LCD.h"
 #include "SCI0.h"
-
 void _Vsci0_RxItr(void);
-
+void SlowTxt(char *);
+char *strl = "Tell me A story.";
 void main(void)
 {
   /* put your own code here */
-
+  LCD_Init();
   _DISABLE_COP();
   SCI0_Init19200();
   EnableInterrupts;
   SCI0CR2_RIE = 1;
   for (;;)
   {
-    asm wai;
+    SlowTxt(strl);
     //_FEED_COP(); /* feeds the dog */
-  }              /* loop forever */
+  } /* loop forever */
   /* please make sure that you never leave main */
 }
 
@@ -31,4 +33,18 @@ interrupt VectorNumber_Vsci0 void _Vsci0_RxItr(void)
     SCI0DRL = temp;
   }
   return;
+}
+void SlowTxt(char *str)
+{
+  char x = 0, row = 0;
+  LCD_Pos(0, 0);
+  x = 0;
+  do
+  {
+    Delay_C(200);
+   // if (str[x] == '\n')
+     // LCD_Pos(++row, 0);
+    //else
+      LCD_Char(str[x++]);
+  } while (str[x]);
 }
