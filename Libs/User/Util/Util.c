9d12fbf1e046c7deabcad0ded0f1ec1c9004ab66
Allyn Nelson <allynnelson13@gmail.com>
2019-10-31 18:16:15 -0600

    Missed 1 function on last commit

--- Libs/User/Util/Util.c
+++ Libs/User/Util/Util.c
@@ -1,10 +1,30 @@
-unsigned long int Util_Binary_To_BCD(unsigned long int val)
+
+
+#define _M_Util_Binary_To_BCD(val, res)   \
+    char i;                               \
+    for (i = 0; i < sizeof(val) * 2; i++) \
+    {                                     \
+        result += (val % 10) << i * 4;    \
+        val = val / 10;                   \
+    }
+
+unsigned long int Util_Binary_To_BCD_uLI(unsigned long int val)
 {
     unsigned long int result;
-    char i;
-    for ( i = 0; i < 8; i++)
-    {
-        result += (val%10)<<i*4;
-    }
-    
+    _M_Util_Binary_To_BCD(val, result);
+    return result;
+}
+
+unsigned int Util_Binary_To_BCD_uI(unsigned int val)
+{
+    unsigned int result;
+    _M_Util_Binary_To_BCD(val, result);
+    return result;
+}
+
+unsigned char Util_Binary_To_BCD_uC(unsigned char val)
+{
+    unsigned char result;
+    _M_Util_Binary_To_BCD(val, result);
+    return result;
 }
