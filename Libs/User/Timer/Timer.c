fa33226e6e0d73249bbec125c63aead84fd9e75b
Allyn Nelson <allynnelson13@gmail.com>
2019-11-01 09:49:57 -0600

    End of Class

--- Libs/User/Timer/Timer.c
+++ Libs/User/Timer/Timer.c
@@ -15,7 +15,35 @@ void Timer_Init8us(void)
     TCTL2 |= 1;
     TFLG1 |= 1;
 }
+void Timer_Init_1us(void)
+{
+    TSCR1 |= 0x80;
+    TSCR2 &= 0XF8;
+    TSCR2 |= 3;    //i think this should leave the rest of the register alone sets our prescale to 64
+    TIOS |= 1;     //set IOS to output Compare
+    TCTL2 &= 0XFC; //enable toggle tWo lines
+    TCTL2 |= 1;
+    TFLG1 |= 1;
+}
+void Timer_Init_125ns(void)
+{
+    TSCR1 |= 0x80;
+    TSCR2 &= 0XF8;
+    TSCR2 |= 0;    //i think this should leave the rest of the register alone sets our prescale to 64
+    TIOS |= 1;     //set IOS to output Compare
+    TCTL2 &= 0XFC; //enable toggle tWo lines
+    TCTL2 |= 1;
+    TFLG1 |= 1;
+}
 
-//void Sleep_ms(unsigned int time)
-//{
-//}
+void Sleep_ms(unsigned int iTime)
+{
+    unsigned int iCount;
+    TC0 = TCNT + 125;
+    for (iCount = 0; iCount < iTime; iCount++)
+    {
+        TFLG1 |= 1;
+        while (TFLG1 & 1);
+        TC0+=125;
+    }
+}
