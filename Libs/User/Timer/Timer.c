8b724d6e3899ab96bce5f8239028bfd423d08b62
Allyn Nelson <allynnelson13@gmail.com>
2019-11-01 09:36:31 -0600

    Capture timer startup complete

---
+++ Libs/User/Timer/Timer.c
@@ -0,0 +1,21 @@
+#include "Timer.h"
+#include "derivative.h"
+#include <stdlib.h>
+#define CLOCK (8000 * 1000)
+#define CLOCK_nS ((1000 * 1000 * 1000) / (CLOCK))
+#define MICRO_PRESCALE(timeNs) ((timeNs) / CLOCK_nS)
+//CAPTURE TIMER
+void Timer_Init8us(void)
+{
+    TSCR1 |= 0x80;
+    TSCR2 &= 0XF8;
+    TSCR2 |= 6;    //i think this should leave the rest of the register alone sets our prescale to 64
+    TIOS |= 1;     //set IOS to output Compare
+    TCTL2 &= 0XFC; //enable toggle tWo lines
+    TCTL2 |= 1;
+    TFLG1 |= 1;
+}
+
+//void Sleep_ms(unsigned int time)
+//{
+//}
