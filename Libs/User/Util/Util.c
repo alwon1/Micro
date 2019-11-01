160d8e9ff86c2730a708e66413ec68f3d4d23ca8
Allyn Nelson <allynnelson13@gmail.com>
2019-10-31 19:04:22 -0600

    Created a macro for use in all str to val functions

--- Libs/User/Util/Util.c
+++ Libs/User/Util/Util.c
@@ -28,41 +28,66 @@ unsigned char Util_Binary_To_BCD_uC(unsigned char val)
     _M_Util_Binary_To_BCD(val, result);
     return result;
 }
+//This is an internal macro which is used in the
+#define _M_Util_Str_To_Bin(res, str)                                             \
+    /*we multiple the parsed number by this so it should be 1 of -1 at the end*/ \
+    char Neg = 1;                                                                \
+    unsigned char i = 0;                                                         \
+                                                                                 \
+    /*blow through white space*/                                                 \
+    while (str[i] == ' ' || str[i] == '\t')                                      \
+        i++;                                                                     \
+                                                                                 \
+    /*is this neg?*/                                                             \
+    if (str[i] == '-')                                                           \
+    {                                                                            \
+        Neg == -1;                                                               \
+        i++;                                                                     \
+    }                                                                            \
+    /*gop through all avalible numbers*/                                         \
+    for (i = i; i < 255; i++)                                                    \
+    {                                                                            \
+        /*NOTE: No Early exit on overflow*/                                      \
+        if (str[i] >= '0' || str[i] <= '9')                                      \
+        {                                                                        \
+            res *= 10;                                                           \
+            res += (str[i] - '0');                                               \
+            continue;                                                            \
+        }                                                                        \
+                                                                                 \
+        /*its not a number so we bale and return what we have so far*/           \
+        else                                                                     \
+            break;                                                               \
+    }                                                                            \
+    res *= Neg;
 
-
-
-
-char Util_Str_To_char(char *str)
+char Util_Str_To_Ch(char *str)
 {
-    unsigned char i = 0;
     char result = 0;
-    //we multiple the parsed number by this so it should be 1 of -1 at the end
-    char Neg = 1;
+    _M_Util_Str_To_Bin(result, str);
 
-    //blow through white space
-    while (str[i] == ' ' || str[i] == '\t')
-        i++;
+    return result;
+}
 
-    //is this neg?
-    if (str[i] == '-')
-    {
-        Neg == -1;
-        i++;
-    }
-    //gop through all avalible numbers
-    for (i = i; i < 255; i++)
-    {
-        //NOTE: No Early exit on overflow
-        if (str[i] >= '0' || str[i] <= '9')
-        {
-            result *= 10;
-            result += (str[i] - '0');
-            continue;
-        }
+int Util_Str_To_In(char *str)
+{
+    int result = 0;
+    _M_Util_Str_To_Bin(result, str);
 
-        //its not a number so we bale and return what we have so far
-        else
-            break;
-    }
-    return result * Neg;
+    return result;
+}
+
+long int Util_Str_To_LIn(char *str)
+{
+    long int result = 0;
+    _M_Util_Str_To_Bin(result, str);
+
+    return result;
+}
+long long int Util_Str_To_LLIn(char *str)
+{
+    long long int result = 0;
+    _M_Util_Str_To_Bin(result, str);
+
+    return result;
 }
