13afb5a6d26bb2cd98c543d216b5a8183a614b87
Allyn Nelson <allynnelson13@gmail.com>
2019-11-08 08:06:38 -0700

    ICA16 Finished

--- ICA/ICA_16/Sources/main.c
+++ ICA/ICA_16/Sources/main.c
@@ -3,16 +3,19 @@
 #include "derivative.h" /* derivative-specific definitions */
 #include <hidef.h>      /* common defines and macros */
 
+void TI10ms(void);
 void main(void)
 {
   /* put your own code here */
 
   _DISABLE_COP();
   SevSeg_Init();
-  // EnableInterrupts
+  TI10ms();
+  EnableInterrupts
 
-  for (;;)
+      for (;;)
   {
+    asm wai;
     //_FEED_COP(); /* feeds the dog */
   } /* loop forever */
   /* please make sure that you never leave main */
@@ -21,12 +24,14 @@ void main(void)
 void TI10ms(void)
 {
   Timer_Init8us();
+  TIE = 1;
 }
-void TimInter(void)
+interrupt VectorNumber_Vtimch0 void TimInter(void)
 {
   volatile static unsigned long int count = 0;
   TC0 += 1250;
   TFLG1 = 1;
   SevSeg_Top4(count / 100);
   SevSeg_Bot4(count % 100);
+  count = count >= 999999 ? 0 : count + 1;
 }
