6e95a58657af101b94931131753ce47fab791e3c
Allyn Nelson <allynnelson13@gmail.com>
2020-06-29 22:04:00 -0600

    Fixes to my own inline asm so it likes it

--- Libs/User/Delay/delay.c
+++ Libs/User/Delay/delay.c
@@ -2,24 +2,24 @@
 #define l
 void Delay_C(int mSec) 
 {
-  asm ldx mSec
-  asm Delay:    ldy   #2665
-  asm delay_2:  dbne  y, delay_2
-  asm dbne      x,    Delay  
+  __asm("$1");
+  __asm("$1");
+  __asm("$1");
+  __asm("$1");
   return;
 }
 //~ 20us
 void Short_Delay(void) 
 {
-  asm ldy       #10
-  asm dbne      y,    *
+  __asm("$1");
+  __asm("$1");
   return;
 }
 //Delay of 49.2ms
 void Long_Delay(void) 
 {
-  asm ldy       #0
-  asm dbne      y,    *
+  __asm("$1");
+  __asm("$1");
   return;
 }
 #endif
